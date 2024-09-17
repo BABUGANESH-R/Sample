eade82005950b650ad504811f48fd0e8a77014c0
Babu Ganesh R <106017721+BABUGANESH-R@users.noreply.github.com>
2024-09-17 12:57:30 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,29 @@
+public class Demo {
+
+    int ruby;
+
+    public static void main(String args[]) {
+
+        int a = 10;
+        while (true) {
+
+        }
+    }
+
+    public int add(int a, int b) {
+        this.ruby = a + b;
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        this.ruby = a - b;
+        return a - b;
+    }
+
+    /**
+     * Display
+     */
+    public void display() {
+        System.out.println("Hello");
+    }
+}
